197f0149bd6b309047893b1971ecb40008869450
David Buckley <buckley.w.david@gmail.com>
2020-07-18 11:21:39 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Arhive.org Link Converter
+Firefox extension based on https://github.com/alexyorke/se_archiveorg_link_restorer/blob/master/se_archiveorg_link_restorer.js
+
+## Attributions
+Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
---
+++ content_scripts/archivify.js
@@ -0,0 +1,30 @@
+function dateToYMDHH(date) {
+    const dateStr = date.toISOString()
+    const compact = dateStr.replaceAll('-','').replaceAll(':','').replace('T','')
+    return compact.substr(0, compact.indexOf('.'))
+}
+
+(function() {
+  /**
+   * Check and set a global guard variable.
+   * If this content script is injected into the same page again,
+   * it will do nothing next time.
+   */
+  if (window.hasRun) {
+    return;
+  }
+  window.hasRun = true;
+
+  const answers = Array.from(document.querySelector('#answers').querySelectorAll('.answer'))
+  answers.forEach((answer) => {
+    const datetime = answer.querySelector('time').getAttribute('datetime')
+    const links = Array.from(answer.querySelectorAll('a'))
+    links.forEach((link) => {
+      const href = link.href
+      if (href.startsWith('http')) {
+        const archiveurl = "https://web.archive.org/web/" + dateToYMDHH(new Date(datetime)) + "/" + href;
+        link.href = archiveurl;
+      }
+    })
+  })
+})()
